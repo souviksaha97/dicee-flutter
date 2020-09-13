3d17c80626a882b821c0bf97f3f60084723cd349
souviksaha97 <souvikssaha@gmail.com>
2020-09-13 19:13:30 +0530

    Completed the second module

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int leftDieState = 1;
+  int rightDieState = 1;
+
+  void getNumber() {
+    rightDieState = Random().nextInt(6) + 1;
+    leftDieState = Random().nextInt(6) + 1;
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    getNumber();
+                  });
+                },
+                child: Image.asset('images/dice$leftDieState.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    getNumber();
+                  });
+                },
+                child: Image.asset('images/dice$rightDieState.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
